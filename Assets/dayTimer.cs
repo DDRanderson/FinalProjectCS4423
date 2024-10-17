ba73a89eb1f7d6d23cbaa3108e9a6de8561f8679
DDRanderson <95324842+DDRanderson@users.noreply.github.com>
2024-10-17 18:18:31 -0500

    Update dayTimer.cs

--- Assets/dayTimer.cs
+++ Assets/dayTimer.cs
@@ -9,12 +9,16 @@ public class dayTimer : MonoBehaviour
 
     public TMP_Text clockText;
 
-    public int clockShownTime = 0;
+    public string clockStartShownTime = "9:00";
+    public int realTimer = 0;
+    public int gameSeconds = 0;
+    public int gameMinutes = 9;
+    public string meridiem = "AM";
  
     void Start()
     {
         //clockText.text = DateTime.Now.ToString();
-        clockText.text = clockShownTime.ToString();
+        clockText.text = clockStartShownTime;
         StartCoroutine(TimerCouroutine());
     }
 
@@ -28,14 +32,27 @@ public class dayTimer : MonoBehaviour
         while(true)
         {
             yield return new WaitForSeconds(1);
-            updateClockText();
+            realTimer++;
+            //each 7 real seconds = 15 game minutes
+            //each day is 252 seconds (9am to 6pm)
+            if (realTimer % 2 == 0){
+                gameSeconds+=15;
+                updateClockText();
+            }
         }
 
     }
 
     public void updateClockText()
     {
-        clockShownTime ++;
-        clockText.text = clockShownTime.ToString();
+        if(gameSeconds % 60 == 0){
+            gameMinutes++;
+            gameSeconds = 0;
+        }
+        if(gameMinutes % 13 == 0){
+            gameMinutes = 1;
+            meridiem = "PM";
+        }
+        clockText.text = gameMinutes.ToString() + ":" + gameSeconds.ToString("00") + " " + meridiem;
     }
 }
