1d9d167e9d5198f3d28ad9c536bb150cc16db914
DDRanderson <95324842+DDRanderson@users.noreply.github.com>
2024-10-21 13:22:11 -0500

    money manager added

--- Assets/dayTimer.cs
+++ Assets/dayTimer.cs
@@ -9,22 +9,23 @@ public class dayTimer : MonoBehaviour
 
     public TMP_Text clockText;
 
-    public string clockStartShownTime = "9:00";
+    public string clockStartShownTime = "9:00 AM";
     public int realTimer = 0;
-    public int gameSeconds = 0;
-    public int gameMinutes = 9;
+    public int gameMinutes = 0;
+    public int gameHours = 9;
     public string meridiem = "AM";
+
+    static List<String> Days = new List<String>{"Mon","Tue","Wed","Thu","Fri"};
+    int iDay = 0;
+    string day = Days[0];
+    
+    
  
     void Start()
     {
         //clockText.text = DateTime.Now.ToString();
-        clockText.text = clockStartShownTime;
+        clockText.text = day + " - " + clockStartShownTime;
         StartCoroutine(TimerCouroutine());
-    }
-
-    void Update()
-    {
-
     }
 
     IEnumerator TimerCouroutine()
@@ -35,8 +36,8 @@ public class dayTimer : MonoBehaviour
             realTimer++;
             //each 7 real seconds = 15 game minutes
             //each day is 252 seconds (9am to 6pm)
-            if (realTimer % 2 == 0){
-                gameSeconds+=15;
+            if (realTimer % 7 == 0){
+                gameMinutes +=15;
                 updateClockText();
             }
         }
@@ -45,14 +46,38 @@ public class dayTimer : MonoBehaviour
 
     public void updateClockText()
     {
-        if(gameSeconds % 60 == 0){
-            gameMinutes++;
-            gameSeconds = 0;
+        if(gameMinutes % 60 == 0){
+            gameHours ++;
+            gameMinutes = 0;
         }
-        if(gameMinutes % 13 == 0){
-            gameMinutes = 1;
+        if(gameHours % 13 == 0){
+            gameHours = 1;
             meridiem = "PM";
         }
-        clockText.text = gameMinutes.ToString() + ":" + gameSeconds.ToString("00") + " " + meridiem;
+        if(realTimer % 252 == 0){
+            gameMinutes = 0;
+            gameHours = 9;
+            meridiem = "AM";
+            realTimer = 0;
+            /*
+            for(int i = 0; i < Days.Count; i++){
+                string temp = Days[i];
+                if(temp == day){
+                    day = Days[i++];
+                }
+            }
+            */
+            iDay++;
+            if (iDay >= Days.Count){
+                iDay = 0;
+            }
+            day = Days[iDay];
+        }
+        clockText.text = day + " - " + gameHours.ToString() + ":" + gameMinutes.ToString("00") + " " + meridiem;
     }
+
+    public int getRealTimer(){
+        return realTimer;
+    }
+
 }
