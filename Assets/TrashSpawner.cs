df1a645ea0c63781d0773b33736c71e2b3c85196
DDRanderson <95324842+DDRanderson@users.noreply.github.com>
2024-10-23 15:05:35 -0500

    trash spawner updated
    trash now attempts to spawn once every second at a 30% of success. time manager now has delegate for a one second event that other objects can now subscribe to.

--- Assets/TrashSpawner.cs
+++ Assets/TrashSpawner.cs
@@ -7,11 +7,13 @@ public class TrashSpawner : MonoBehaviour
 
     public GameObject trash;
     public float radius = 1;
+    public int percentChance = 30;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //subscribe to oneSecondTrigger event
+        FindObjectOfType<TimeManager>().oneSecondEvent += SpawnObjectRandomly;
     }
 
     // Update is called once per frame
@@ -26,6 +28,17 @@ public class TrashSpawner : MonoBehaviour
         Vector3 randomPos = Random.insideUnitCircle * radius;
 
         Instantiate(trash, transform.position + randomPos, Quaternion.identity);
+    }
+
+    void SpawnObjectRandomly(){
+        //percentage chance of a trash spawning
+        int rand = Random.Range(0,100);
+        Debug.Log("Random Trash Spawn Number: " + rand);
+        
+        if(rand <= percentChance){
+            Vector3 randomPos = Random.insideUnitCircle * radius;
+            Instantiate(trash, transform.position + randomPos, Quaternion.identity);
+        }
     }
 
     private void OnDrawGizmos(){
